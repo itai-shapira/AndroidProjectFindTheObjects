f218da4c67d2b7b4d90566d7401c14675682f8a2
itaiz <itai.zvi.shapira@gmail.com>
2025-03-08 21:25:56 +0200

    Changed Database

--- app/src/main/java/com/example/schoolproject/HelperDB.java
+++ app/src/main/java/com/example/schoolproject/HelperDB.java
@@ -12,11 +12,12 @@ import java.util.ArrayList;
 // Handles the Users table
 public class HelperDB extends SQLiteOpenHelper {
     // Table information
-    public static final String DB_FILE = "pass_game.db";
+    public static final String DB_FILE = "find_the_objects.db";
     public static final String USERS_TABLE = "Users";
     public static final String USER_NAME = "UserName";
     public static final String USER_PWD = "UserPassword";
     public static final String USER_PHONE = "UserPhone";
+    public static final String USER_FOUND_OBJECTS = "UserFoundObjects";
 
     public HelperDB(@Nullable Context context) {
         super(context, DB_FILE, null, 1);
@@ -38,7 +39,8 @@ public class HelperDB extends SQLiteOpenHelper {
         String st = "CREATE TABLE IF NOT EXISTS "+USERS_TABLE;
         st += "("+USER_NAME+" TEXT, ";
         st += USER_PWD+" TEXT, ";
-        st += USER_PHONE+" TEXT);";
+        st += USER_PHONE+" TEXT, ";
+        st += USER_FOUND_OBJECTS+" TEXT);";
         return st;
     }
 
@@ -46,7 +48,8 @@ public class HelperDB extends SQLiteOpenHelper {
     public ArrayList<User> getAllRecords() {
         SQLiteDatabase db;
         int index;
-        String name, pwd, phone;
+        String name, pwd, phone, found_objects;
+        Boolean[6]
         db = this.getReadableDatabase();
         ArrayList<User> list = new ArrayList<>();
 
@@ -59,11 +62,32 @@ public class HelperDB extends SQLiteOpenHelper {
             pwd = cursor.getString(index);
             index = cursor.getColumnIndex(USER_PHONE);
             phone = cursor.getString(index);
-            User record = new User(name, pwd, phone);
+            index = cursor.getColumnIndex(USER_FOUND_OBJECTS);
+            found_objects = cursor.getString(index);
+            User record = new User(name, pwd, phone, convertFoundObjects(found_objects));
             list.add(record);
         }
 
         db.close();
         return list;
+    }
+
+    public static Boolean[] convertFoundObjects(String strFoundObjects) {
+        Boolean[] foundObjects = new Boolean[6];
+        for (int i = 0; i < strFoundObjects.length(); i++) {
+            if ((int)strFoundObjects.charAt(i) == 1)
+                foundObjects[i] = true;
+        }
+        return foundObjects;
+    }
+    public static String convertFoundObjects(Boolean[] foundObjects) {
+        String strFoundObjects = "";
+        for (boolean b : foundObjects) {
+            if (b)
+                strFoundObjects = strFoundObjects + 1;
+            else
+                strFoundObjects = strFoundObjects + 0;
+        }
+        return strFoundObjects;
     }
 }
--- app/src/main/java/com/example/schoolproject/User.java
+++ app/src/main/java/com/example/schoolproject/User.java
@@ -3,11 +3,19 @@ package com.example.schoolproject;
 // Handles Users
 public class User {
     private String userName, userPwd, userLastName, userPhone;
+    private Boolean[] userFoundObjects;
 
+    public User(String userName, String userPwd, String userPhone, Boolean[] userFoundObjects) {
+        this.userName = userName;
+        this.userPwd = userPwd;
+        this.userPhone = userPhone;
+        this.userFoundObjects = userFoundObjects;
+    }
     public User(String userName, String userPwd, String userPhone) {
         this.userName = userName;
         this.userPwd = userPwd;
         this.userPhone = userPhone;
+        userFoundObjects = new Boolean[6];
     }
     public User(String userName, String userPwd) {
         this.userName = userName;
@@ -30,5 +38,11 @@ public class User {
     }
     public void setUserPhone(String userPhone) {
         this.userPhone = userPhone;
+    }
+    public Boolean[] getUserFoundObjects() {
+        return userFoundObjects;
+    }
+    public void setUserFoundObjects(Boolean[] userFoundObjects) {
+        this.userFoundObjects = userFoundObjects;
     }
 }
